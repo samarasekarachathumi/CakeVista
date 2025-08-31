7e7508ec1bb913134f8826a10d80c933850e5c4c
samarasekarachathumi <110670083+samarasekarachathumi@users.noreply.github.com>
2025-09-01 00:14:38 +0530

    remove unwanted files

--- Backend/Routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createUser, loginUser } from '../Controllers/userController.js';
-
-const userRouter = express.Router();
-
-userRouter.post('/', createUser)
-userRouter.post("/login",loginUser)
-
-export default userRouter;
--- Frontend/src/assets/assets.js
+++
@@ -1,9 +0,0 @@
-import search from './Searchicon.png';
-import profile from './profile.png';
-import cart from './cart.png';
-
-export const assets ={
-    search,
-    profile,
-    cart
-}
